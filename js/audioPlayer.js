3899446e7cbf9d393d7abe218ca30511221e8508
equus71 <grzegorz.biziel@gmail.com>
2015-09-13 19:38:29 +0200

    Change formatting Add on $destroy handler

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,40 +1,47 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular.module('anniversary-photo-app')
+  angular.module('anniversary-photo-app')
 
-        .directive('audioPlayer', function ($ionicPlatform, appDataService) {
-            return {
-                link: function (scope, elem, attrs) {
-                    var myMedia = null;
-                    $ionicPlatform.ready(function () {
-                        appDataService.getAppData().then(function(data){
-                            initializeMusic(data.music);
-                        });
-                    });
-                    scope.$on('musicStop', pause);
-                    scope.$on('musicStart', play);
-
-                    function initializeMusic(music){
-                        if (music.enabled){
-                            myMedia = new Media(music.path);
-                            myMedia.play();
-                        }
-                    }
-
-                    function pause() {
-                        if (myMedia) {
-                            myMedia.pause();
-                        }
-                    }
-
-                    function play() {
-                        if (myMedia) {
-                            myMedia.play();
-                        }
-                    }
-                }
-            };
+  .directive('audioPlayer', function($ionicPlatform, appDataService) {
+    return {
+      link: function(scope, elem, attrs) {
+        var myMedia = null;
+        $ionicPlatform.ready(function() {
+          appDataService.getAppData().then(function(data) {
+            initializeMusic(data.music);
+          });
         });
+        scope.$on('musicStop', pause);
+        scope.$on('musicStart', play);
+        scope.$on('$destroy', cleanUp);
+
+        function initializeMusic(music) {
+          if (music.enabled) {
+            myMedia = new Media(music.path);
+            myMedia.play();
+          }
+        }
+
+        function pause() {
+          if (myMedia) {
+            myMedia.pause();
+          }
+        }
+
+        function play() {
+          if (myMedia) {
+            myMedia.play();
+          }
+        }
+
+        function cleanUp() {
+          if (myMedia) {
+            myMedia.release();
+          }
+        }
+      }
+    };
+  });
 
 })();
